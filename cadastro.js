b015dd7789d98b17b548c52f5dc19fbfa16779e1
Bruno Gabriel <149634079+Brunogarbs@users.noreply.github.com>
2024-05-14 16:34:27 -0300

    Alteração no <img> e window.location.href

--- cadastro.js
+++ cadastro.js
@@ -1,24 +1,30 @@
 let password = document.getElementById('password');
 let Confirmpassword = document.getElementById('Confirmpassword');
+let firstNameInput = document.getElementById('firstname');
 
 function validarpassword() {
-  if (password.value != Confirmpassword.value) {
-    Confirmpassword.setCustomValidity("Senhas diferentes!");
-    Confirmpassword.reportValidity();
-    return false;
-  } else {
-    Confirmpassword.setCustomValidity("");
-    return true;
-  }
+    if (password.value != Confirmpassword.value) {
+        Confirmpassword.setCustomValidity("Senhas diferentes!");
+        Confirmpassword.reportValidity();
+        return false;
+    } else {
+        Confirmpassword.setCustomValidity("");
+        return true;
+    }
 }
 
-// verificar também quando o campo for modificado, para que a mensagem suma quando as passwords forem iguais
 Confirmpassword.addEventListener('input', validarpassword);
 
-function SalvaNome(){
-    
-    // Salva o primeiro nome no localStorage
-    localStorage.setItem("firstName", firstName);
-    
+function SalvaNome() {
+    if (validarpassword()) {
+        // Salva o primeiro nome no localStorage
+        localStorage.setItem("firstName", firstNameInput.value);
+        // Redireciona para a página upload.html
+        window.location.href = "upload.html";
+    }
+}
 
-  }
+document.getElementById("continue-button").addEventListener("click", function(event){
+    event.preventDefault();
+    SalvaNome();
+});
