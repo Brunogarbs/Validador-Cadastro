5653f4774a2a6c278760725299b1bf7207605575
Bruno Gabriel da Silva <bjuvencio44@gmail.com>
2024-05-15 17:08:42 -0300

    Validação inputs

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,6 @@
 document.getElementById('cadastroForm').addEventListener('submit', function(event) {
     event.preventDefault();  // Impede o envio do formulário padrão
+    
 
     let password = document.getElementById('password');
     let Confirmpassword = document.getElementById('Confirmpassword');
