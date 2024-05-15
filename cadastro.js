1dcbf12ed4ec8a7abce1d68b259e6a4b9d2852b7
Bruno Gabriel da Silva <bjuvencio44@gmail.com>
2024-05-15 16:23:14 -0300

    Ajuste validação de campos

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Impede o envio do formulário padrão
+
+    let password = document.getElementById('password');
+    let Confirmpassword = document.getElementById('Confirmpassword');
+    let firstNameInput = document.getElementById('firstname');
+    let form = event.target;
+
+    function validarpassword() {
+        if (password.value !== Confirmpassword.value) {
+            Confirmpassword.setCustomValidity("Senhas diferentes!");
+            Confirmpassword.reportValidity();
+            return false;
+        } else {
+            Confirmpassword.setCustomValidity("");
+            return true;
+        }
+    }
+
+    if (form.checkValidity() && validarpassword()) {
+        // Salva o primeiro nome no localStorage
+        localStorage.setItem("firstName", firstNameInput.value);
+        // Redireciona para a página upload.html
+        window.location.href = "upload.html";
+    }
+    
+});
