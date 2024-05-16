3bb19947fe2831c8d63309650653fb78ffaecf5b
Bruno Gabriel da Silva <bjuvencio44@gmail.com>
2024-05-16 10:04:41 -0300

    Iserção de mascara no formato do telefone e validação de formato

--- cadastro.js
+++ cadastro.js
@@ -1,30 +1,51 @@
-document.getElementById('cadastroForm').addEventListener('submit', function(event) {
-    event.preventDefault();  // Impede o envio do formulário padrão
+$(document).ready(function(){
+    // Aplica a máscara de telefone
+    $('#number').mask('(00) 00000-0000');
 
-    let password = document.getElementById('password');
-    let Confirmpassword = document.getElementById('Confirmpassword');
-    let firstNameInput = document.getElementById('firstname');
-    let form = event.target;
+    document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+        event.preventDefault();  // Impede o envio do formulário padrão
 
-    function validarpassword() {
-        if (password.value !== Confirmpassword.value) {
-            Confirmpassword.setCustomValidity("Senhas diferentes!");
-            Confirmpassword.reportValidity();
-            return false;
-        } else {
-            Confirmpassword.setCustomValidity("");
-            return true;
+        let password = document.getElementById('password');
+        let confirmpassword = document.getElementById('confirmpassword');
+        let firstNameInput = document.getElementById('firstname');
+        let phoneNumberInput = document.getElementById('number');
+        let form = event.target;
+
+        function validarpassword() {
+            if (password.value !== confirmpassword.value) {
+                confirmpassword.setCustomValidity("Senhas diferentes!");
+                confirmpassword.reportValidity();
+                return false;
+            } else {
+                confirmpassword.setCustomValidity("");
+                return true;
+            }
         }
-    }
 
-    // Adiciona um listener ao campo de confirmação de senha para validar em tempo real
-    Confirmpassword.addEventListener('input', validarpassword);
-    
-    // Verifica se o formulário é válido e se as senhas coincidem antes de prosseguir
-    if (form.checkValidity() && validarpassword()) {
-        // Salva o primeiro nome no localStorage
-        localStorage.setItem("firstName", firstNameInput.value);
-        // Redireciona para a página upload.html
-        window.location.href = "upload.html";
-    }
+        function validarNumeroTelefone() {
+            // Verifica se o número de telefone está no formato correto
+            let telefoneRegex = /^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/;
+            if (!telefoneRegex.test(phoneNumberInput.value)) {
+                phoneNumberInput.setCustomValidity("Formato inválido de telefone!");
+                phoneNumberInput.reportValidity();
+                return false;
+            } else {
+                phoneNumberInput.setCustomValidity("");
+                return true;
+            }
+        }
+
+        // Adiciona um listener ao campo de confirmação de senha para validar em tempo real
+        confirmpassword.addEventListener('input', validarpassword);
+        // Adiciona um listener ao campo de número de telefone para validar em tempo real
+        phoneNumberInput.addEventListener('input', validarNumeroTelefone);
+        
+        // Verifica se o formulário é válido e se as senhas coincidem antes de prosseguir
+        if (form.checkValidity() && validarpassword() && validarNumeroTelefone()) {
+            // Salva o primeiro nome no localStorage
+            localStorage.setItem("firstName", firstNameInput.value);
+            // Redireciona para a página upload.html
+            window.location.href = "upload.html";
+        }
+    });
 });
